2b3dcece07ce463360f2028242dfe33df0ce2f78
BatuhanPC <batuhanluleci@outlook.com>
2024-05-25 11:54:27 +0300

    ball spawn feature finished.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : Singleton<GameManager>
+using Unity.Netcode;
+public class GameManager : NetworkSingleton<GameManager>
 {
+    public GameObject BallPrefab;
     public Transform ClientOneSpawnPoint;
     public Transform ClientTwoSpawnPoint;
+
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+
+            PlayersManager.Instance.OnPlayerCountTwo.AddListener(StartGame);
+
+        }
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("game started.");
+        SpawnBall();
+    }
+
+    private void SpawnBall()
+    {
+        var instance = Instantiate(BallPrefab);
+
+        var instanceNetworkObject = instance.GetComponent<NetworkObject>();
+        instanceNetworkObject.Spawn();
+    }
 }
--- Assets/PlayersManager.cs
+++ Assets/PlayersManager.cs
@@ -1,10 +1,12 @@
 using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayersManager : NetworkSingleton<PlayersManager>
 {
     NetworkVariable<int> playersCount = new NetworkVariable<int>();
+    public UnityEvent OnPlayerCountTwo; 
     public GameObject Paddle;
     public int PlayerCount
     {
@@ -18,11 +20,17 @@ public class PlayersManager : NetworkSingleton<PlayersManager>
         {
             playersCount.Value++;
             Debug.Log($"Player {id} is connected.");
-            var instance = Instantiate(NetworkManager.GetNetworkPrefabOverride(Paddle), 
+            var instance = Instantiate(NetworkManager.GetNetworkPrefabOverride(Paddle),
                                        (playersCount.Value == 1) ? GameManager.Instance.ClientOneSpawnPoint : GameManager.Instance.ClientTwoSpawnPoint);
 
             var instanceNetworkObject = instance.GetComponent<NetworkObject>();
             instanceNetworkObject.SpawnAsPlayerObject(id);
+
+            if (playersCount.Value == 2)
+            {
+                Debug.Log("started");
+                OnPlayerCountTwo.Invoke();
+            }
         };
         NetworkManager.Singleton.OnClientDisconnectCallback += (id) =>
         {
