f4045e61967bd24a5fb14b857b03f7df13224043
BatuhanPC <batuhanluleci@outlook.com>
2024-05-25 10:37:54 +0300

    InGame Debug Console added. ParrelSync added. Paddle Initialization added. Paddle Movement added.

--- Assets/PlayersManager.cs
+++ Assets/PlayersManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayersManager : NetworkSingleton<PlayersManager>
 {
     NetworkVariable<int> playersCount = new NetworkVariable<int>();
-
+    public GameObject Paddle;
     public int PlayerCount
     {
         get { return playersCount.Value; }
@@ -13,18 +13,24 @@ public class PlayersManager : NetworkSingleton<PlayersManager>
 
     private void Start()
     {
-       
-            NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
-            {
-                playersCount.Value++;
-                Debug.Log($"Player {id} is connected.");
-            };
-            NetworkManager.Singleton.OnClientDisconnectCallback += (id) =>
-            {
-                playersCount.Value--;
-                Debug.Log($"Player {id} is disconnected.");
-            };
-        
+
+        NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
+        {
+            playersCount.Value++;
+            Debug.Log($"Player {id} is connected.");
+            var instance = Instantiate(NetworkManager.GetNetworkPrefabOverride(Paddle), 
+                                       (playersCount.Value == 1) ? GameManager.Instance.ClientOneSpawnPoint : GameManager.Instance.ClientTwoSpawnPoint);
+
+            var instanceNetworkObject = instance.GetComponent<NetworkObject>();
+            instanceNetworkObject.SpawnAsPlayerObject(id);
+        };
+        NetworkManager.Singleton.OnClientDisconnectCallback += (id) =>
+        {
+            playersCount.Value--;
+            Debug.Log($"Player {id} is disconnected.");
+
+        };
+
 
     }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,25 +1,70 @@
+ï»¿using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
+public class Paddle : NetworkBehaviour
 {
-    public float speed = 5f; // Raketin hareket hýzý
+    public float speed = 5f; // Raketin hareket hÃ½zÃ½
     public float minY = -3.5f; // Raketin minimum y konumu
     public float maxY = 3.5f; // Raketin maksimum y konumu
 
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private NetworkVariable<float> YAxisInputValue = new NetworkVariable<float>();
+
+    public override void OnNetworkSpawn()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Rigidbody olustu");
+    }
     // Update is called once per frame
     void Update()
     {
-        // Yön tuþlarý veya diðer giriþlerle raketin hareketini kontrol et
+        if (IsClient && IsOwner)
+        {
+            GetClientInput();
+        }
+        // YÃ¶n tuÃ¾larÃ½ veya diÃ°er giriÃ¾lerle raketin hareketini kontrol et
+
+        //float moveAmount = moveInput * speed * Time.deltaTime;
+
+        //// Raketin yeni pozisyonunu hesapla
+        //float newYPosition = transform.position.y + moveAmount;
+
+        //// Yeni pozisyonu sÃ½nÃ½rla
+        //newYPosition = Mathf.Clamp(newYPosition, minY, maxY);
+
+        //// Raketin konumunu gÃ¼ncelle
+        //transform.position = new Vector2(transform.position.x, newYPosition);
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsServer)
+        {
+
+
+            // Rigidbody Ã¼zerinde yalnÄ±zca y ekseni hareketini uygula
+
+            Vector2 movement = new Vector2(0, YAxisInputValue.Value * speed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + movement);
+
+            //// Yeni pozisyonu sÄ±nÄ±rla
+            //Vector2 clampedPosition = new Vector2(rb.position.x, Mathf.Clamp(rb.position.y, minY, maxY));
+            //rb.MovePosition(clampedPosition);
+        }
+    }
+
+    private void GetClientInput()
+    {
         float moveInput = Input.GetAxisRaw("Vertical");
-        float moveAmount = moveInput * speed * Time.deltaTime;
+        SendClientInputServerRPC(moveInput);
+    }
 
-        // Raketin yeni pozisyonunu hesapla
-        float newYPosition = transform.position.y + moveAmount;
-
-        // Yeni pozisyonu sýnýrla
-        newYPosition = Mathf.Clamp(newYPosition, minY, maxY);
-
-        // Raketin konumunu güncelle
-        transform.position = new Vector2(transform.position.x, newYPosition);
+    [ServerRpc]
+    private void SendClientInputServerRPC(float moveInput)
+    {
+        YAxisInputValue.Value = moveInput;
     }
 }
